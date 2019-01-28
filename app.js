7c8f87d21a62605d11aeb49d01ca03239ae673bd
unknown <sralinkous@gmail.com>
2019-01-28 11:40:28 -0800

    Add mongoose database, causing error

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// set up mongoose database
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://localhost/test';
+mongoose.connect(mongoDB);
+mongoose.Prommise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+db.once('open', function(){
+
+});
+
 // view engine setup
 // app.set('views', path.join(_dirname, 'views'));
 app.set('view engine', 'pug');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
