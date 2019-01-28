4169ea4a403c0d206127ac24ae423be765761c3c
unknown <sralinkous@gmail.com>
2019-01-27 18:21:52 -0800

    Add first view with pug

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
+// app.set('views', path.join(_dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
